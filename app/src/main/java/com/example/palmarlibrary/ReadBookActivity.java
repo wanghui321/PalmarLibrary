59e11b33314cccec7e788368f01d5481d00e5352
wanghui <1562995735@qq.com>
2018-05-15 16:54:10 +0800

    5.15：已读图书页面的编写

---
+++ app/src/main/java/com/example/palmarlibrary/ReadBookActivity.java
@@ -0,0 +1,19 @@
+package com.example.palmarlibrary;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Administrator on 2018/5/15.
+ */
+
+public class ReadBookActivity extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.read_book_layout);
+
+    }
+}
