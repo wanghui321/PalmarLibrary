ad2d1747154d73862890e0277d1842c74e16af4e
wanghui <1562995735@qq.com>
2018-05-16 10:46:12 +0800

    5.16查看书评界面代码的编写

---
+++ app/src/main/java/com/example/palmarlibrary/BookReviewActivity.java
@@ -0,0 +1,19 @@
+package com.example.palmarlibrary;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Administrator on 2018/5/16.
+ */
+
+public class BookReviewActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.book_review_layout);
+
+
+    }
+}
