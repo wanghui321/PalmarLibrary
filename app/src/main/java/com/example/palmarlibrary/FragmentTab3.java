e598d748948c5007fbfd6534a848fe3cb80f9e13
郭彭 <627829348@qq.com>
2018-05-29 19:09:18 +0800

    类型筛选部分

--- app/src/main/java/com/example/palmarlibrary/FragmentTab3.java
+++ app/src/main/java/com/example/palmarlibrary/FragmentTab3.java
@@ -1,11 +1,18 @@
 package com.example.palmarlibrary;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2018/5/21.
@@ -19,5 +26,49 @@ public class FragmentTab3 extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.collection_type_layout,container,false);
         return view;
+    }
+
+    public class TypeNameAdapter extends BaseAdapter {
+        private Context context;
+        private int item_layout_id;
+        private List<Map<String,Object>> dataSource;
+
+        public TypeNameAdapter (Context context,int item_layout_id,List<Map<String,Object>> dataSource){
+            this.context = context;
+            this.item_layout_id = item_layout_id;
+            this.dataSource = dataSource;
+        }
+
+        @Override
+        public int getCount() {
+            return dataSource.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return dataSource.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null){
+                convertView = LayoutInflater.from(context).inflate(item_layout_id,null);
+            }
+
+            Log.e("typeName",dataSource.get(position).get("typeName").toString());
+
+
+            TextView tv_typeName = convertView.findViewById(R.id.tv_typeName);
+
+
+            tv_typeName.setText(dataSource.get(position).get("typeName").toString());
+
+
+            return convertView;
+        }
     }
 }
