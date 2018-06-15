914ee7dd97743af8e834aeb961f113352da10977
郭彭 <627829348@qq.com>
2018-06-15 09:31:24 +0800

    6.15

--- app/src/main/java/com/example/palmarlibrary/AdviseActivity.java
+++ app/src/main/java/com/example/palmarlibrary/AdviseActivity.java
@@ -7,6 +7,17 @@ import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * Created by ruiwang on 2018/5/22.
@@ -20,11 +31,14 @@ public class AdviseActivity extends Activity {
 
         ImageView adviseback = findViewById(R.id.advise_back);
         Button advisesubmit = findViewById(R.id.advise_submit);
+        final TextView advisetv = findViewById(R.id.et_advise);
 
         advisesubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String advise = advisetv.getText().toString();
                 Intent intent = new Intent();
+                intent.putExtra("advise",advise);
                 intent.setClass(AdviseActivity.this,AdviseSubmitLoadingActivity.class);
                 startActivity(intent);
             }
